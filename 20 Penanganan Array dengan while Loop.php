4ba059f81232366effe0796553de396b4694da3d
novlarahmulia <rahmulianovla@gmail.com>
2024-08-31 17:34:36 +0700

    penanganan array dengan while loop

---
+++ 20 Penanganan Array dengan while Loop.php
@@ -0,0 +1,10 @@
+<?php
+$fruits = ["apple", "banana", "cherry"];
+$i = 0;
+
+while ($i < count($fruits)) {
+    echo $fruits[$i] . "<br><hr>";
+    $i++;
+}
+print_r("<p><strong>by :novla rahmulia");
+?>
