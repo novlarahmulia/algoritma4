96983c1267f35310cc47b479de33610baa552cac
novlarahmulia <rahmulianovla@gmail.com>
2024-09-01 22:20:22 +0700

    penggunaan continue

---
+++ 28 Penggunaan continue.php
@@ -0,0 +1,9 @@
+<?php
+for ($i = 0; $i < 10; $i++) {
+    if ($i % 2 === 0) {
+        continue;
+    }
+    echo $i . "<br><hr> ";
+}
+// Output: 1 3 5 7 9
+print_r("<p><strong>by :novla rahmulia");
