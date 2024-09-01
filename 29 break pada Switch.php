d53ed2e56f94e0502b98c949f6a89ea5f986631b
novlarahmulia <rahmulianovla@gmail.com>
2024-09-01 22:25:28 +0700

    break pada switch

---
+++ 29 break pada Switch.php
@@ -0,0 +1,19 @@
+<?php
+$buah = "manggis";
+
+switch ($buah) {
+    case "mangga":
+        echo "Ini mangga";
+        break;
+    case "apel":
+        echo "Ini manggis";
+        // Break disini menghentikan switch
+        break;
+    case "pisang":
+        echo "Ini pisang";
+        break;
+    default:
+        echo "Buah ini sangat dikenal";
+}
+print_r("<p><strong>by :novla rahmulia");
+?>
