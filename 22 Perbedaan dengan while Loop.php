29d38f98186148c9da42776e3dc30f8b866903f0
novlarahmulia <rahmulianovla@gmail.com>
2024-09-01 21:42:30 +0700

    contoh kondisi loop yang benar

---
+++ 22 Perbedaan dengan while Loop.php
@@ -0,0 +1,9 @@
+<?php
+$hitung = 1;
+
+while ($hitung <= 5) {
+    echo "Angka: $hitung <br><hr>";
+    $hitung++;
+}
+print_r("<p><strong>by :novla rahmulia");
+?>
---
+++ 23 Contoh Kondisi Loop yang Benar.php
@@ -0,0 +1,9 @@
+<?php
+$hitung = 1;
+
+do {
+    echo "Angka: $hitung <br><hr>";
+    $hitung++;
+} while ($hitung <= 8);
+print_r("<p><strong>by :novla rahmulia");
+?>
