4389944e7e8e37bee828c0a0228be34a53a424b1
novlarahmulia <rahmulianovla@gmail.com>
2024-09-01 22:05:39 +0700

    foreach loop dengan key

---
+++ 25 Foreach Loop dengan Key.php
@@ -0,0 +1,8 @@
+<?php
+$harga = array("apel" => 10000, "pisang" => 5000, "ceri" => 15000);
+
+foreach ($harga as $kunci => $nilai) {
+    echo "Harga dari " . $kunci . " adalah " . $nilai . "<br><hr>";
+}
+print_r("<p><strong>by :novla rahmulia");
+?>
