1e984494e48f198a72ed97fca82a4c010044d95e
novlarahmulia <rahmulianovla@gmail.com>
2024-09-01 22:15:52 +0700

    penggunaan break

---
+++ 27 Penggunaan break.php
@@ -0,0 +1,11 @@
+<?php
+for ($i = 0; $i < 10; $i++) {
+    if ($i === 10) {
+        break;
+    }
+    echo $i . "<br><hr> ";
+}
+
+// Output: 0 1 2 3 4 
+print_r("<p><strong>by :novla rahmulia");
+?>
