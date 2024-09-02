dc1613a4ff107a2a5ddec4bde50a0fe9e2090860
novlarahmulia <rahmulianovla@gmail.com>
2024-09-02 22:08:33 +0700

    menambahkan array asosiatif

---
+++ 37 menambahkan array asosiatif.php
@@ -0,0 +1,11 @@
+<?php
+$email["subjek"] = "Apa Kabar?";
+$email["pengirim"] = "idan@rahmulianovla.com";
+$email["isi"] = "Apa kabar? sudah lama tidak berkabar";
+
+echo "<pre>";
+
+print_r($email);
+echo "</pre>";
+print_r("<p><strong>by :novla rahmulia");
+?>
