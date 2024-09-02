240da87b70930cd9b4dbfff846312d2e1ccb829b
novlarahmulia <rahmulianovla@gmail.com>
2024-09-02 21:54:23 +0700

    menambahkan isi array pada indeks

---
+++ 35 menambahkan isi array pada indeks.php
@@ -0,0 +1,19 @@
+<?php
+// membuat array
+$user = [
+    "dian",
+    "muhar",
+    "novlarahmulia"
+];
+
+// mengisi array pada indek ke-1 ("muhar")
+$user[1] = "Novla";
+
+// mengisi array pada indek ke-2 ("dian")
+$user[2] = "muhar";
+// mencetak isi array
+echo "<pre>";
+print_r($user);
+echo "</pre>";
+print_r("<p><strong>by :novla rahmulia");
+?>
