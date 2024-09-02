09e859023fad1fdb8f630e49ab13fbe5b74461df
novlarahmulia <rahmulianovla@gmail.com>
2024-09-02 22:12:12 +0700

    array indexed

---
+++ 38 array indexed .php
@@ -0,0 +1,6 @@
+<?php
+  $nama_variabel=array("nama1"=>"80","nama2"=>"76","nama3"=>"90");
+  echo "hasil dari program " . $nama_variabel['nama1'] . " nilai.";
+  print_r("<p><strong>by :novla rahmulia");
+
+?>
