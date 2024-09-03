6c1054a360febe934390f0603b8320ad935ba6e4
novlarahmulia <rahmulianovla@gmail.com>
2024-09-03 21:27:21 +0700

    menyertakan nilai key

---
+++ 40 Menyertakan nilai key.php
@@ -0,0 +1,7 @@
+<?php
+$bulan = array ('Januari', 'Februari', 'Maret', 'april');
+foreach ($bulan as $index => $nama_bulan) {
+	echo ($index + 1) . '. ' . $nama_bulan . '<br/><hr>';
+}
+print_r("<p><strong>by :novla rahmulia");
+?>
