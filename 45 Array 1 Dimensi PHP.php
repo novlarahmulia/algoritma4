4d8610ef455dc041cbfcd3d2ad67b4bee1fccf94
novlarahmulia <rahmulianovla@gmail.com>
2024-09-03 22:21:22 +0700

    array 1 dimensi php

---
+++ 45 Array 1 Dimensi PHP.php
@@ -0,0 +1,5 @@
+<?php
+$test=array("Index pertama","Index Kedua","Index ketiga");
+echo "Hasil array : 0.<br>" . $test[0] . ", 1.<br>" . $test[1] . " dan 2.<br>" . $test[2] . ".";
+print_r("<p><strong>by :novla rahmulia");
+?>
